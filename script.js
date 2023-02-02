10da5731fc93eec501a9e0ba559025dccd7c6cf0
Kevin Tome <k.tome@unifesp.br>
2023-02-02 19:36:20 -0300

    feat: adding max number of tries & final reset

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const input = document.querySelector('.guess-input')
 const button = document.querySelector('.submit-button')
 
-const secretNum = parseInt(Math.random() * 1001);
+const maxTries = 15
+var secretNum = parseInt(Math.random() * 1001);
+var currenty_try = 0
 
 const validateInput = ({ target }) => {
     if( target.value != '' && target.value > 0){
@@ -13,9 +15,20 @@ const validateInput = ({ target }) => {
 
 const checkGuess = (event) => {
     event.preventDefault();
+    
+    if(currenty_try == maxTries){
+        alert('Only 15 tries! New secret number, try again!')
+        currenty_try = 0
+        secretNum = parseInt(Math.random() * 1001);
+        // console.log(secretNum)
+    }
 
+    currenty_try++
+    // console.log(currenty_try)
     if(input.value == secretNum){
-        alert('Your guess is RIGHT!!')    
+        alert('Your guess is RIGHT! New secret number, try again!')
+        currenty_try = 0
+        secretNum = parseInt(Math.random() * 1001);
     } else if (input.value > secretNum){
         alert('Try lesser...')
     } else if(input.value < secretNum){
@@ -23,11 +36,12 @@ const checkGuess = (event) => {
     }
 }
 
-console.log(secretNum)
+// console.log(secretNum)
+
 input.addEventListener('input', validateInput);
 button.addEventListener('click', checkGuess);
 
-console.log(secretNum)
+
 
 
 
